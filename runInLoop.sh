383927e9f2eec146f54c862f9bec7a978acdc427
wtgodbe <wigodbe@microsoft.com>
2018-04-19 15:41:52 -0700

    Try parallel

--- runInLoop.sh
+++ runInLoop.sh
@@ -2,13 +2,22 @@
 
 count=0
 
-while :
+run_test_in_loop()
+{
+	while :
+	do
+		echo "In loop $count"
+		echo "$1/dotnet xunit.console.netcore.exe $2 -method $3"
+		$1/dotnet xunit.console.netcore.exe $2 -method $3
+		if [[ $? != 0 ]]; then exit; fi
+		((count++))
+	done
+}
+
+for i in $(seq 1 100)
 do
-	echo "In loop $count"
-	echo "$1/dotnet xunit.console.netcore.exe $2 -method $3"
-	$1/dotnet xunit.console.netcore.exe $2 -method $3
-	if [[ $? != 0 ]]; then exit; fi
-	((count++))
+	run_test_in_loop $1 $2 $3
 done
+wait
 
 exit 0

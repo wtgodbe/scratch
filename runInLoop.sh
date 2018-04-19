74f930adf86d4c56eb3c5839565dbee2003bfe3b
wtgodbe <wigodbe@microsoft.com>
2018-04-19 15:52:53 -0700

    Modify parallel

--- runInLoop.sh
+++ runInLoop.sh
@@ -9,12 +9,15 @@ run_test_in_loop()
 		echo "In loop $count"
 		echo "$1/dotnet xunit.console.netcore.exe $2 -method $3"
 		$1/dotnet xunit.console.netcore.exe $2 -method $3
-		if [[ $? != 0 ]]; then exit; fi
+		if [[ $? != 0 ]]; then 
+			touch fail.txt
+			exit 10
+		fi
 		((count++))
 	done
 }
 
-for i in {1..100}
+for i in {1..6}
 do
 	run_test_in_loop $1 $2 $3 &
 done
